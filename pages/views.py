7a4f75daa33c6957b9a41f3ce8a609ac8b0ac801
Esteban Molina Mejia <emolinam@eafit.edu.co>
2025-07-25 15:11:45 -0500

    Creacion y mejora de templates y static files

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
-from django.https
+from django.http import HttpResponse #New
 
 # Create your views here.
+def HomePageView(request):
+    return HttpResponse("Hello World Esteban Molina")
