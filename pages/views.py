05dff3d6ee52ceebfbc765adf11571f779d9c49e
Esteban Molina Mejia <emolinam@eafit.edu.co>
2025-07-26 13:16:52 -0500

    actividad 5

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponse
 from django.views import View
+from django.http import HttpResponseRedirect
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = "pages/home.html"  
@@ -24,10 +25,10 @@ class ContactPageView(TemplateView):
     
 class Product:
     products = [
-    {"id":"1", "name":"TV", "description":"Best TV", "price":"$800"},
-    {"id":"2", "name":"iPhone", "description":"Best iPhone", "price":"$1200"},
-    {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price":"$150"},
-    {"id":"4", "name":"Glasses", "description":"Best Glasses", "price":"$200"}
+    {"id":"1", "name":"TV", "description":"Best TV", "price":800},
+    {"id":"2", "name":"iPhone", "description":"Best iPhone", "price":1200},
+    {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price":150},
+    {"id":"4", "name":"Glasses", "description":"Best Glasses", "price":200},
     ]
     
 class ProductIndexView(View):
@@ -42,8 +43,12 @@ class ProductIndexView(View):
 class ProductShowView(View):
     template_name = 'products/show.html'
     def get(self, request, id):
+        try:
+            product = Product.products[int(id)-1]
+        except(IndexError, ValueError):
+            return HttpResponseRedirect('')
+          
         viewData = {}
-        product = Product.products[int(id)-1]
         viewData["title"] = product["name"] + " - Online Store"
         viewData["subtitle"] = product["name"] + " - Product information"
         viewData["product"] = product
