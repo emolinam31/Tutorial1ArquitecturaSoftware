eb3d5a5cbd0f9464a76496f4a0170d13019b5492
Esteban Molina Mejia <emolinam@eafit.edu.co>
2025-08-13 06:44:01 -0500

    Tutorial 3 Completado con el carrito de compras

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ContactPageView, ProductShowView, ProductIndexView, ProductCreateView
+from .views import HomePageView, AboutPageView, ContactPageView, ProductShowView, ProductIndexView, ProductCreateView, CartView, CartRemoveAllView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
     path('contact/', ContactPageView.as_view(), name='contact'),
-    path('products/', ProductIndexView.as_view(), name='products'),  # Cambié 'index' por 'products'
-    path('products/create/', ProductCreateView.as_view(), name='form'),  # Agregué la barra final
-    path('products/<str:id>/', ProductShowView.as_view(), name='show'),  # Agregué la barra final
+    path('products/', ProductIndexView.as_view(), name='products'),  
+    path('products/create/', ProductCreateView.as_view(), name='form'),  
+    path('products/<str:id>/', ProductShowView.as_view(), name='show'),  
+    path('cart/', CartView.as_view(), name='cart_index'),
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
 ] 
